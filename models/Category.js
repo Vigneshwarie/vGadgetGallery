e8eb5ca727ca6f951ae271eb74d07e92670546d0
Vigneswari Sambandam <vigneshwarie@gmail.com>
2023-12-21 17:07:42 -0500

    Updated fields for Category, Product, ProductTag and Tag based on the sample seeds

--- models/Category.js
+++ models/Category.js
@@ -4,9 +4,19 @@ const sequelize = require('../config/connection.js');
 
 class Category extends Model {}
 
+// Updated Category fields based on the seeds
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull:false,
+    },
   },
   {
     sequelize,
