02db634e67e59b59441f894fc88c3a0b3ac7e7b0
Vigneswari Sambandam <vigneshwarie@gmail.com>
2023-12-23 01:15:31 -0500

    Added functionality to get all tags and its products

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,11 +1,16 @@
 const router = require('express').Router();
 const { Tag, Product, ProductTag } = require('../../models');
 
-// The `/api/tags` endpoint
-
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+// Route to get all Tags and its associated products
+router.get('/', async (req, res) => {
+  try {
+    const tagData = await Tag.findAll({
+      include: {model: Product, through:ProductTag}
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
